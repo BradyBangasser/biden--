c371d683453fda367e648e4db1f587cc77b6bb6f
Brady Bangasser <bradybbangasser@gmail.com>
2023-04-23 21:00:10 -0500

    ahhhhhhhhh

--- src/compiler/lexer.rs
+++ src/compiler/lexer.rs
@@ -1,17 +1,48 @@
 pub mod lexer {
-    use std::any::{Any};
 
     use crate::compiler::{tokenizer::tokenizer::Token, constants::constants::Constants};
 
-    #[derive(Debug)]
+    #[derive(Debug, Clone)]
     struct Variable<T: Sized> {
-        pub name: String,
-        pub val: Box<T>
+        name: String,
+        val: Box<T>,
+        undefined: bool
     }
 
     impl<T: 'static> Variable<T> {
         fn new(name: String, val: Option<Box<T>>) -> Variable<T> {
-            return Variable { name, val: val.unwrap_or(Box::new(unsafe { std::mem::MaybeUninit::zeroed().assume_init() })) };
+            let is_undefined = val.is_none();
+            let unwrapped_val = val.unwrap_or(Box::new(unsafe { std::mem::MaybeUninit::zeroed().assume_init() }));
+            return Variable { name, val: unwrapped_val, undefined: is_undefined };
+        }
+
+        fn set_val(&mut self, new_val: T) {
+            if !self.undefined {
+                self.undefined = false;
+            }
+
+
+            let raw_variable: *mut T = Box::into_raw(self.val);
+            unsafe {
+                raw_variable.write(new_val);
+                self.val = Box::from_raw(raw_variable);
+            }
+        }
+
+        fn get_val(self) -> T {
+            return *self.val;
+        }
+
+        fn name(self) -> String {
+            return self.name;
+        }
+
+        fn is_undefined(self) -> bool {
+            return self.undefined;
+        }
+
+        fn get_boxed_val(self) -> Box<T> {
+            return self.val;
         }
     }
 
@@ -33,18 +64,12 @@ pub mod lexer {
                 i += 1;
 
                 if tokens[i].token != Constants::EQUALS_T {
-                    variables.push(Variable::new(variable_name.to_string(), None))
+                    variables.push(Variable::<i32>::new(variable_name.to_string(), None))
                 }
-
-                variables[0].val = Box::<i32>::new(5);
 
                 println!("{:#?}", variables[0]);
 
-                let new_box: *mut i32 = Box::into_raw(variables[0].val.to_owned());
-
-                unsafe { new_box.write(72) };
-
-                println!("{:?}", variables[0].val = unsafe { Box::from_raw(new_box) });
+                variables[0].set_val(5);
 
                 println!("{:#?}", variables[0]);
                 
